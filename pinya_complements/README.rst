0eb19bcbacef9dffbbb6ad545030242a3f8bc862
Miquel March <mmarch@praxya.com>
2020-03-20 19:59:03 +0100

    [ADD] pinya complements

---
+++ pinya_complements/README.rst
@@ -0,0 +1,35 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+Pinya Complements
+=================
+
+Mòduls complementaris a "Pinta la Pinya"
+
+Installation
+============
+
+* database_cleanup
+* web_no_bubble
+* web_sheet_full_width
+
+Configuration
+=============
+
+Cap
+
+Usage
+=======
+Credits
+=======
+
+Contributors
+------------
+
+* Miquel March <m.marchpuig@gmail.com>
+
+Maintainer
+----------
+
+* Miquel March <m.marchpuig@gmail.com>
---
+++ pinya_complements/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2020 Miquel March <m.marchpuig@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pinta Complements",
+    "summary": "Mòduls complementaris a 'Pinta la Pinya'",
+    "version": "11.0.1.0.1",
+    "category": "Custom",
+    'website': 'http://www.pintalapinya.cat/',
+    "author": "Miquel March",
+    "license": "AGPL-3",
+    "installable": True,
+    'application': False,
+    "depends": [
+        "database_cleanup",
+        "web_no_bubble",
+        "web_sheet_full_width"
+    ],
+    "data": [
+    ],
+}
