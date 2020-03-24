592387041a2df6aa4d277837e7e478dac8e7e8a5
Miquel March <mmarch@praxya.com>
2020-03-24 21:10:04 +0100

    [FIX] Error

--- pinya_complements/__manifest__.py
+++ pinya_complements/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Pinta Complements",
+    "name": "Pinya Complements",
     "summary": "Mòduls complementaris a 'Pinta la Pinya'",
     "version": "11.0.1.0.1",
     "category": "Custom",
