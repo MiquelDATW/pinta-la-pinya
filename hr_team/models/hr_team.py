30201a82c34110cf9a638f9660028791ee14b1d8
Miquel March <mmarch@praxya.com>
2020-04-23 19:33:18 +0200

    [IMP] many2many fields

--- hr_team/models/hr_team.py
+++ hr_team/models/hr_team.py
@@ -19,7 +19,7 @@ class HrTeam(models.Model):
 
     manager_id = fields.Many2one('hr.employee', string='Manager', track_visibility='onchange')
     member_ids = fields.Many2many('hr.employee', string='Members')
-    department_ids = fields.Many2many('hr.department', string='Departments', compute="_compute_department", store=True)
+    department_id = fields.Many2one('hr.department', string='Department')
     member_count = fields.Integer(string='Member count', compute="_compute_member_count", store=True)
 
     @api.multi
@@ -28,14 +28,6 @@ class HrTeam(models.Model):
         teams = self.filtered(lambda x: bool(x.member_ids))
         for team in teams:
             team.member_count = len(team.member_ids.ids)
-
-    @api.multi
-    @api.depends('member_ids', 'member_ids.department_id')
-    def _compute_department(self):
-        teams = self.filtered(lambda x: bool(x.member_ids))
-        for team in teams:
-            depts = team.member_ids.mapped('department_id')
-            team.department_ids = [(6, 0, depts.ids)]
 
     @api.model
     def create(self, vals):
