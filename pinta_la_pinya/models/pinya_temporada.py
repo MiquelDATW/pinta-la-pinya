c835867a25991c170eda11e5bf833411ce50ac02
Miquel March <mmarch@praxya.com>
2020-04-24 18:50:10 +0200

    [ADD] pinya_temporada

--- pinta_la_pinya/models/pinya_temporada.py
+++ pinta_la_pinya/models/pinya_temporada.py
@@ -20,9 +20,9 @@ class PinyaTemporada(models.Model):
     figura_ids = fields.Many2many('pinya.plantilla', string="Figures", compute='_compute_figures', store=True)
 
     muixeranga_ids = fields.One2many('pinya.muixeranga', 'temporada_id', string="Muixerangues")
-    no_oficials_count = fields.Integer(string='Total muix. assajos', compute='_compute_muixerangues_count', store=True)
-    oficials_count = fields.Integer(string='Total muix. oficials', compute='_compute_muixerangues_count', store=True)
-    muixerangues_count = fields.Integer(string='Total muixerangues', compute='_compute_muixerangues_count', store=True)
+    no_oficials_count = fields.Integer(string='Figures assajos', compute='_compute_muixerangues_count', store=True)
+    oficials_count = fields.Integer(string='Figures oficials', compute='_compute_muixerangues_count', store=True)
+    muixerangues_count = fields.Integer(string='Total figures', compute='_compute_muixerangues_count', store=True)
 
     actuacio_ids = fields.One2many('pinya.actuacio', 'temporada_id', string="Actuacions")
     actuacions_count = fields.Integer(string='Total actuacions', compute='_compute_actuacions_count', store=True)
@@ -52,33 +52,25 @@ class PinyaTemporada(models.Model):
     @api.depends('muixeranga_ids', 'muixeranga_ids.plantilla_id')
     def _compute_figures(self):
         for temporada in self:
-            figures = temporada.muixeranga_ids.mapped('plantilla_id')
+            actuacio = temporada.muixeranga_ids.filtered(lambda x: x.actuacio_id.tipus == 'actuacio')
+            figures = actuacio.mapped('plantilla_id')
             temporada.figura_ids = [(6, 0, figures.sorted('name').ids)]
 
     def mostrar_oficials(self):
-        view_tree_id = self.env.ref('pinta_la_pinya.view_muixeranga_tree_all').id
-        view_form_id = self.env.ref('pinta_la_pinya.view_muixeranga_form').id
-        oficials = self.muixeranga_ids.filtered(lambda x: x.actuacio_id.tipus == 'actuacio')
-        domain = [('id', 'in', oficials.ids)]
-        name = "Muix. Oficials {}".format(self.name)
-        action = {
-            'type': 'ir.actions.act_window',
-            'views': [(view_tree_id, 'tree'), (view_form_id, 'form')],
-            'view_mode': 'form',
-            'name': name,
-            'target': 'current',
-            'res_model': 'pinya.muixeranga',
-            'context': {},
-            'domain': domain,
-        }
-        return action
+        res = self.mostrar_pinya_muixeranga('actuacio')
+        return res
 
     def mostrar_no_oficials(self):
+        res = self.mostrar_pinya_muixeranga('assaig')
+        return res
+
+    def mostrar_pinya_muixeranga(self, tipus):
         view_tree_id = self.env.ref('pinta_la_pinya.view_muixeranga_tree_all').id
         view_form_id = self.env.ref('pinta_la_pinya.view_muixeranga_form').id
-        no_oficials = self.muixeranga_ids.filtered(lambda x: x.actuacio_id.tipus == 'assaig')
-        domain = [('id', 'in', no_oficials.ids)]
-        name = "Muix. Assajos {}".format(self.name)
+        pinya_muixeranga = self.muixeranga_ids.filtered(lambda x: x.actuacio_id.tipus == tipus)
+        domain = [('id', 'in', pinya_muixeranga.ids)]
+        tipus2 = "Oficials" if tipus == 'actuacio' else "Assajos"
+        name = "Muix. {} {}".format(tipus2, self.name)
         action = {
             'type': 'ir.actions.act_window',
             'views': [(view_tree_id, 'tree'), (view_form_id, 'form')],
@@ -92,29 +84,20 @@ class PinyaTemporada(models.Model):
         return action
 
     def mostrar_actuacions(self):
-        view_tree_id = self.env.ref('pinta_la_pinya.view_actuacio_tree').id
-        view_form_id = self.env.ref('pinta_la_pinya.view_actuacio_form').id
-        actuacions = self.actuacio_ids.filtered(lambda x: x.tipus == 'actuacio')
-        domain = [('id', 'in', actuacions.ids)]
-        name = "Actuacions {}".format(self.name)
-        action = {
-            'type': 'ir.actions.act_window',
-            'views': [(view_tree_id, 'tree'), (view_form_id, 'form')],
-            'view_mode': 'form',
-            'name': name,
-            'target': 'current',
-            'res_model': 'pinya.actuacio',
-            'context': {},
-            'domain': domain,
-        }
-        return action
+        res = self.mostrar_pinya_actuacio('actuacio')
+        return res
 
     def mostrar_assajos(self):
+        res = self.mostrar_pinya_actuacio('assaig')
+        return res
+
+    def mostrar_pinya_actuacio(self, tipus):
         view_tree_id = self.env.ref('pinta_la_pinya.view_actuacio_tree').id
         view_form_id = self.env.ref('pinta_la_pinya.view_actuacio_form').id
-        assajos = self.actuacio_ids.filtered(lambda x: x.tipus == 'assaig')
-        domain = [('id', 'in', assajos.ids)]
-        name = "Assajos {}".format(self.name)
+        actuacions = self.actuacio_ids.filtered(lambda x: x.tipus == tipus)
+        domain = [('id', 'in', actuacions.ids)]
+        tipus2 = "Actuacions" if tipus == 'actuacio' else "Assajos"
+        name = "{} {}".format(tipus2, self.name)
         action = {
             'type': 'ir.actions.act_window',
             'views': [(view_tree_id, 'tree'), (view_form_id, 'form')],
