396104b5036d84c51c357012593dae8831ebada9
Miquel March <mmarch@praxya.com>
2020-05-31 13:09:18 +0200

    [DEL] hr_employee_age & hr_team

--- mm_hr_team/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2020 Miquel March <m.marchpuig@gmail.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import hr_employee
-from . import hr_team
